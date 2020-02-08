c0b22c4bc55fb9cebf93d6f537d1b60d89c716db
nkymyuta <54033388+nkymyuta@users.noreply.github.com>
2020-02-08 16:18:54 +0900

    Update State.java

--- src/main/java/frc/robot/State.java
+++ src/main/java/frc/robot/State.java
@@ -2,13 +2,27 @@ package frc.robot;
 
 public class State {
 
+    
+    //根本的な制御モード（ドライブ、セル発射、コンパネぐるぐる、ぶら下がり）
+    public enum ControlState{
+        m_Drive,
+        m_Firingball,
+        m_Panelrotation,
+        m_Hanging
+
+    }
+    
+    //ドライブモード
     public enum DriveState{
         kManual,
         kAuto,
         kTest
     }
 
+    //State宣言
+    public ControlState  controlState;
     public DriveState driveState;
+    
     public double driveStraightSpeed, driveRotateSpeed;  //Driveの速度
     public double driveStraightSetpoint, driveRotateSetpoint;    // PID制御の目標値
     public boolean is_drivePIDOn;    // PID制御するかどうか
@@ -18,6 +32,9 @@ public class State {
     }
 
     public void stateInit(){
+
+        //基本制御モードはドライブモード
+        controlState = ControlState.m_Drive;
 
         //DriveのStateを初期化
         driveState = DriveState.kManual;
