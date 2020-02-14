5244e20fa3c74092d87aaeee07c4f853158baee0
nkymyuta <54033388+nkymyuta@users.noreply.github.com>
2020-02-14 18:24:23 +0900

    Update State.java

--- src/main/java/frc/robot/State.java
+++ src/main/java/frc/robot/State.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 public class State {
 
-    
     //根本的な制御モード（ドライブ、セル発射、コンパネぐるぐる、ぶら下がり）
     public enum ControlState{
         m_Drive,
@@ -11,25 +10,53 @@ public class State {
         m_Hanging
 
     }
-    
-    //ドライブモード
+
+    //Driveのモード定義
     public enum DriveState{
         kManual,
         kAuto,
         kTest
     }
 
+    //Canonの状態を確認
+    public enum CanonState{
+        k_Basic,           //基本状態（最も下を向いている）
+        k_Parallel,        //地面と平行な状態
+        k_Aaiming,         //砲台の照準を合わせている状態
+        k_Maxup            //最も上を向いている状態
+    }
+
+    //Shooterの処理を分ける
+    public enum ShooterState{
+        s_Intake,           //ボールを取り込む（ドライブモード）
+        s_Shoot,            //ボールを発射する（セル発射モード）
+        s_Panelrotate       //コンパネを回す　（パネル回転モード）
+    }
+
     //State宣言
     public ControlState  controlState;
     public DriveState driveState;
-    
+    public CanonState canonState;
+    public ShooterState shooterState;
+
+    //MotorのSpeed＆PID宣言
     public double driveStraightSpeed, driveRotateSpeed;  //Driveの速度
     public double driveStraightSetpoint, driveRotateSetpoint;    // PID制御の目標値
-    public boolean is_drivePIDOn;    // PID制御するかどうか
+    public boolean is_drivePIDOn;    // PID制御しているかどうか
+
+    public double CanonRotateSpeed;  //砲台の回る速度
+    public double CanonRotateSetPoint;  // PID制御の目標値
+    public boolean is_canonPIDOn;  // PID制御しているかどうか
+
+    //入力の認識変数宣言
+    public boolean is_CanonChangeBasic; //砲台の角度初期化するかどうか
+
 
     State(){
         stateInit();
     }
+
+
 
     public void stateInit(){
 
@@ -42,6 +69,19 @@ public class State {
         driveRotateSpeed = 0;
         is_drivePIDOn = false;
 
+        //ArmのStateを初期化
+        canonState = CanonState.k_Basic;
+        CanonRotateSpeed = 0;
+        CanonRotateSetPoint = 0;
+        is_canonPIDOn = false;
+
+        shooterState = ShooterState.s_Intake;
+
+        //入力の認識変数の初期化
+        is_CanonChangeBasic = false;
+
     }
+
+
 
 }
