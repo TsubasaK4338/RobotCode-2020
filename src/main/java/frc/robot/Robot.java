d1ae9f75e0c30f0b8a8ccb27172eaa0e5c56ecb2
nkymyuta <54033388+nkymyuta@users.noreply.github.com>
2020-02-14 18:31:37 +0900

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,18 +9,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 //TalonSRX&VictorSPXのライブラリー
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 
 public class Robot extends TimedRobot {
 
   //コントローラー
   //とりあえず、Xbox2つ
-  XboxController driver, operator;
+ XboxController driver, operator;
 
   //モーター(全11個)
     
@@ -37,52 +40,62 @@ public class Robot extends TimedRobot {
     Encoder ShootEncoderRight , ShootEncoderLeft;
 
     //インテイクモーター（ロボット先端のアレ）
-    VictorSPX IntakeMotor;
+    WPI_VictorSPX IntakeMotor;
     
     //アーム内部のベルトモーター
-    VictorSPX BeltMotorFront , BeltMotorback;
+    WPI_VictorSPX BeltMotorFront , BeltMotorback;
 
     //ぶら下がり用のモーター&エンコーダー
     WPI_TalonSRX HangingMotor;
     Encoder HangingEncoder;
   
-
+  
   //センサー
   ADXRS450_Gyro gyro;
 
+  //マイクロスイッチ(砲台の角度上限＆下限)
+  DigitalInput MaxUpSwitch;
+  DigitalInput MaxDownSwitch;
+ 
   //SubClass
   Drive drive;
+  Canon canon;
+  Shooter shooter;
   State state;
 
+
+//ロボットに搭載するハードウェアの宣言（初期設定）
   @Override
   public void robotInit() {
-
+    
     //コントローラーの初期化
-    driver = new XboxController(Const.DriveControllerPort);
-    operator = new XboxController(Const.OperateControllerPort);
+    driver = new XboxController(0);
+    operator = new XboxController(1);
 
     //gyroの初期化
     gyro = new ADXRS450_Gyro();
 
-    //----------------------------------------------------------------
-    //DriveMotor
-    //ドライブモーターの初期化
-    driveRightFront = new WPI_TalonSRX(Const.DriveRightFrontPort);
-    driveRightBack = new VictorSPX(Const.DriveRightBackPort);
-    driveLeftFront = new WPI_TalonSRX(Const.DriveLeftFrontPort);
-    driveLeftBack = new VictorSPX(Const.DriveLeftBackPort);
-    //ドライブモーターの台形加速&フォローの設定
-    driveLeftFront.configOpenloopRamp(Const.DriveFullSpeedTime);
-    driveLeftBack.follow(driveLeftFront);
-    driveRightFront.configOpenloopRamp(Const.DriveFullSpeedTime);
-    driveRightBack.follow(driveRightFront);
+    //---------------------------------------------------------------
+    //DriveMotor  
     
+      //ドライブモーターの初期化
+      driveRightFront = new WPI_TalonSRX(Const.DriveRightFrontPort);
+      driveRightBack = new VictorSPX(Const.DriveRightBackPort);
+      driveLeftFront = new WPI_TalonSRX(Const.DriveLeftFrontPort);
+      driveLeftBack = new VictorSPX(Const.DriveLeftBackPort);
+      //ドライブモーターの台形加速&フォローの設定
+      driveLeftFront.configOpenloopRamp(Const.DriveFullSpeedTime);
+      driveLeftBack.follow(driveLeftFront);
+      driveRightFront.configOpenloopRamp(Const.DriveFullSpeedTime);
+      driveRightBack.follow(driveRightFront);
+
     //----------------------------------------------------------------
     //ArmMotor
     
       //アームの角度変更モーター&エンコーダーの初期化
       CanonMotor = new WPI_TalonSRX(Const.CanonMotorPort);
       CanonEncoder = new Encoder(Const.CanonEncoderPort_A , Const.CanonEncoderPort_B);
+      //CanonMotor.getSelectedSensorPosition();
       //アームの角度変更の台形加速設定
       CanonMotor.configOpenloopRamp(Const.CanonFullSpeedTime);
 
@@ -93,11 +106,11 @@ public class Robot extends TimedRobot {
       ShootEncoderRight = new Encoder(Const.ShootEncoderRightPort_A , Const.ShootEncoderRightPort_B);
 
       //インテイクモーターの初期化
-      IntakeMotor = new VictorSPX(Const.IntakeMotorPort);
+      IntakeMotor = new WPI_VictorSPX(Const.IntakeMotorPort);
 
       //アーム内部のベルトモーターの初期化
-      BeltMotorFront = new VictorSPX(Const.BeltMotorFrontPort);
-      BeltMotorback  = new VictorSPX(Const.BeltMotorBackPort);
+      BeltMotorFront = new WPI_VictorSPX(Const.BeltMotorFrontPort);
+      BeltMotorback  = new WPI_VictorSPX(Const.BeltMotorBackPort);
 
       //ぶら下がり用モーター＆エンコーダーの初期化
       HangingMotor = new WPI_TalonSRX(Const.HangingMotorPort);
@@ -105,29 +118,44 @@ public class Robot extends TimedRobot {
 
     //----------------------------------------------------------------
 
+    //マイクロスイッチの初期化
+    MaxUpSwitch   = new DigitalInput(Const.MaxUpSwitchPort);
+    MaxDownSwitch = new DigitalInput(Const.MaxDownSwitchPort);
 
-    drive = new Drive(driveLeftFront, driveRightFront, gyro);
+
+    //サブクラスの初期化
     state = new State();
-
+    drive = new Drive(driveLeftFront, driveRightFront, gyro);
+    canon = new Canon(CanonMotor, MaxUpSwitch, MaxDownSwitch);
+    shooter = new Shooter(ShootMotorLeft, ShootMotorRight);
+    
   }
 
+ 
   @Override
   public void robotPeriodic() {
   }
 
+  
   @Override
   public void autonomousInit() {
+    
   }
+
 
   @Override
   public void autonomousPeriodic() {
+  
   }
 
+
+  //メインループ
   @Override
   public void teleopPeriodic() {
-    
-    //この関数はずっとループして呼ばれるので、ループの最初にモード確認するお！
-    
+
+    //まず、最初に砲台の状態を確認
+    canon.CanonCheck(state);
+
     //根本的な制御モードの区別、そして入力確認
     switch(state.controlState) {
       
@@ -136,13 +164,13 @@ public class Robot extends TimedRobot {
       //【ドライブモード】
       case m_Drive:
         
-        //OperatorのLBボタンが押されたら、セル発射モードへ切り替え
+        //OperatorのLBボタンが押されている間、セル発射モードへ切り替え
         if(operator.getBumper(Hand.kLeft)){
           state.controlState = State.ControlState.m_Firingball;
           break;
         }
         
-        //OperatorのBackボタンが押されたら、パネル回転モードへ切り替え
+        //OperatorのBackボタンが押されている間、パネル回転モードへ切り替え
         if(operator.getBackButton()){
           state.controlState = State.ControlState.m_Panelrotation;
           break;
@@ -153,20 +181,21 @@ public class Robot extends TimedRobot {
           state.controlState = State.ControlState.m_Hanging;
           break;
         }
-    
-        /********** Drive ***********/
-          state.driveState = State.DriveState.kManual;
-          state.driveStraightSpeed = Util.deadbandProcessing(driver.getY(Hand.kLeft));
-          state.driveRotateSpeed = Util.deadbandProcessing(driver.getX(Hand.kRight));
 
-        
-       //ここはてらゆうにまかせようじゃあないか！！
+        //driverのAボタンが押されたら、砲台の角度を初期化
+        state.is_CanonChangeBasic = operator.getAButton();
+
+
+        /********** Drive ***********/
+        state.driveState = State.DriveState.kManual;
+        state.driveStraightSpeed = Util.deadbandProcessing(driver.getY(Hand.kLeft));
+        state.driveRotateSpeed = Util.deadbandProcessing(driver.getX(Hand.kRight));
+
         break;
       
-     //---------------------------------------------------------------------------------
-    
-     //【セル発射モード】
-     case m_Firingball:
+      //---------------------------------------------------------------------------------
+      //【セル発射モード】
+        case m_Firingball:
 
         //OperatorのLBボタンが離されたら、ドライブモードへ切り替え
         if(operator.getBumperReleased(Hand.kLeft)){
@@ -174,13 +203,17 @@ public class Robot extends TimedRobot {
           break;
         }
 
-          //ここはぼくががんばる
+        /********** CanonRotate ***********/
+        state.CanonRotateSpeed = Util.deadbandProcessing(operator.getY(Hand.kLeft));
+
+
+          
         break;
 
-     //---------------------------------------------------------------------------------
+      //---------------------------------------------------------------------------------
       
-     //【コンパネぐるぐるモード】
-     case m_Panelrotation:
+      //【コンパネぐるぐるモード】
+      case m_Panelrotation:
 
         //OperatorのBackボタンが離されたら、ドライブモードへ切り替え
         if(operator.getBackButtonReleased()){
@@ -188,14 +221,12 @@ public class Robot extends TimedRobot {
           break;
         }
 
-          //ここをしゅｎｙに任せようじゃあないか！！
-
         break;
 
-     //---------------------------------------------------------------------------------
+      //---------------------------------------------------------------------------------
       
-     //【ぶら下がりモード】
-     case m_Hanging:
+      //【ぶら下がりモード】
+      case m_Hanging:
 
         //driverのBackボタンがおされたら、ドライブモードへ切り替え
         if(driver.getBackButtonPressed()){
@@ -203,20 +234,21 @@ public class Robot extends TimedRobot {
           break;
         }
 
-          //ぶら下がりの仕様がよくわからん…てらゆう、お前知ってるんだって？じゃぁお前担当な！（もしくは女王）
-
         break;
     }
-    //---------------------------------------------------------------------------------
-    
-    //入力確認が終わったら最後にStateを確認（apply）しよう
 
-   　drive.apllyState(state);
-    
-    
+    //入力確認が終わったら最後にStateを適用（apply）して出力処理
+    drive.apllyState(state);
+    canon.applyState(state);
+    shooter.applyState(state);
+
   }
 
+  /**
+   * This function is called periodically during test mode.
+   */
   @Override
   public void testPeriodic() {
+
   }
 }
