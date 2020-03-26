173c01c14cdc715f35943defed180ba630dbcc40
Hirose-Yuto <heroandmanto0202@gmail.com>
2020-03-27 00:13:00 +0900

    test

--- src/main/java/frc/robot/PanelRotationMode.java
+++ src/main/java/frc/robot/PanelRotationMode.java
@@ -7,13 +7,13 @@ import frc.robot.subClass.*;
 public class PanelRotationMode {
     Drive drive;
     Panel panel;
-    Arm   arm;
+    Arm arm;
     XboxController driver, operator;
 
     PanelRotationMode(Drive drive, Panel panel, Arm arm, Controller controller) {
         this.drive = drive;
         this.panel = panel;
-        this.arm   = arm;
+        this.arm = arm;
         this.driver   = controller.driver;
         this.operator = controller.operator;
     }
